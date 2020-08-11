22c8cf700d0cb8c6a5b1d6bbacec3986449295c0
Douglas <douglas.contactpro@gmail.com>
2020-08-11 20:59:33 -0300

    clean up

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const previews = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".full-img");
 const caption = document.querySelector(".caption");
 
+// Add open event to each image
 previews.forEach(preview => {
   preview.addEventListener('click', () => {
     modal.classList.add("open");
     original.classList.add("open");
     
-    //Dynamic change text and image
+    // Dynamic change image
     const originalSrc = preview.getAttribute("data-original");
     original.src = `./img/full/${originalSrc}`
-    
+    // Change image caption
     const altText = preview.alt;
     caption.textContent = altText;
   });
 });
 
+// Close image popup
 modal.addEventListener('click', (e) => {
   if (e.target.classList.contains("modal")) {
     modal.classList.remove("open");
